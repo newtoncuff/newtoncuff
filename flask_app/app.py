9b0306d2e3cad3c27e6bf523255eb57e5477f51e
Newton <nalanzo2001@gmail.com>
2025-03-23 20:31:08 +0000

    added navigation/layout.html, thoughts.html, delusions.html, passions.html, interests.html and also edited the app.py to navigate to them

--- flask_app/app.py
+++ flask_app/app.py
@@ -26,6 +26,33 @@ def home():
         content = f"Error: {e}"
     return render_template("home.html", title=title, content=content)
 
+@app.route('/thoughts')
+def navigation_thoughts():
+    titleHeader = "Thoughts"
+    titleSubHeader = "Try and keep up with the random thoughts that invade my head and lead to the randomness that ensues."
+    content = "This is the content for the Thoughts page"
+    return render_template('navigation/thoughts.html', titleHeader=titleHeader, titleSubHeader=titleSubHeader, content=content)
+
+@app.route('/passions')
+def navigation_passions():
+    titleHeader = "Passions"
+    titleSubHeader = "I treasures these more than words can describe. They are my joy, frustration and provide motivation."
+    content = "This is the content for the Passions page"
+    return render_template('navigation/passions.html', titleHeader=titleHeader, titleSubHeader=titleSubHeader, content=content)
+
+@app.route('/delusions')
+def navigation_delusions():
+    titleHeader = "Delusions"
+    titleSubHeader = "These are my ramblings manifested into words, my theories and ideas. Here you will find everything else."
+    content = "This is the content for the Delusions page"
+    return render_template('navigation/delusions.html', titleHeader=titleHeader, titleSubHeader=titleSubHeader, content=content)
+
+@app.route('/interest')
+def navigation_interests():
+    titleHeader = "Interest"
+    titleSubHeader = "My interests have been described as seasonal so follow me as i grow, learn, explore, experiment and discover."
+    content = "This is the content for the Interest page"
+    return render_template('navigation/interest.html', titleHeader=titleHeader, titleSubHeader=titleSubHeader, content=content)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
