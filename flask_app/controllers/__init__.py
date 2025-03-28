c90532af39950972d179dabff7151bc9501c02f8
Newton <nalanzo2001@gmail.com>
2025-03-28 00:40:41 +0000

    Added admin page and functionality

--- flask_app/controllers/__init__.py
+++ flask_app/controllers/__init__.py
@@ -5,9 +5,11 @@ def register_controllers(app):
     from controllers.thoughts import thoughts_controller
     from controllers.passions import passions_controller
     from controllers.interests import interests_controller
+    from controllers.admin import admin_controller
     
     app.register_blueprint(main_controller)
     app.register_blueprint(delusions_controller)
     app.register_blueprint(thoughts_controller)
     app.register_blueprint(passions_controller)
     app.register_blueprint(interests_controller)
+    app.register_blueprint(admin_controller)
