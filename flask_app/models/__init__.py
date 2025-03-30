1db02269bc54f8c42385dec530f182314ccf50a8
Newton <nalanzo2001@gmail.com>
2025-03-30 21:20:29 +0000

    *removed the database class in the init.py file *removed uused reference in the mindobject file

--- flask_app/models/__init__.py
+++ flask_app/models/__init__.py
@@ -1,16 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-from .user import User
-
-
-class Database:
-    def __init__(self):
-        self.users = User.query.all()
-
-    def get_users(self):
-        return self.users
-
-    def refresh(self):
-        self.users = User.query.all()
--- flask_app/models/mindObjects.py
+++ flask_app/models/mindObjects.py
@@ -1,5 +1,4 @@
 from . import db
-from sqlalchemy.ext.declarative import declared_attr
 
 class MindObject(db.Model):
     """
